bd2aab85c758319ede99adc24e7ba613f9b0ac68
Damian Ryan <github@damianryan.com>
2018-04-03 12:33:41 +0100

    Externalised dependency versions as properties and added Dokka plugin for documentation. Switch repository from maven central to jcenter.

--- src/main/kotlin/com/damianryan/shinypotato/Allocation.kt
+++ src/main/kotlin/com/damianryan/shinypotato/Allocation.kt
@@ -1,5 +1,11 @@
 package com.damianryan.shinypotato
 
+/**
+ * Allocation of all or some quantity of lots of one of more trades to a client end account, corresponding to a CCP
+ * position account.
+ * @author Damian Ryan
+ * @since January 2018
+ */
 data class Allocation(val id: String,
                       val quantity: Int,
                       val trades: List<Trade>,
